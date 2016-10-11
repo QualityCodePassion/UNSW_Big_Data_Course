b1a3b7564a1284c6552887a2ae90c488baaa1ab6
Tim Hale <hale_timo@hotmail.com>
2016-10-11 16:53:36 +1100

    proj - Finished Q2, but it's a hack that should be fixed if I get time

--- ass3/src/comp9313/ass3/Problem1.scala
+++ ass3/src/comp9313/ass3/Problem1.scala
@@ -14,13 +14,26 @@ object Ass3 {
   val CreationTime = 4
 
   /** A function that splits a line of input into (voteTypeId, postId) tuples. */
-  def parseLine(line: String) = {
+  def q1parseLine(line: String) = {
     val fields = line.split(",")
     val voteTypeId = fields(VoteTypeId).toInt
     val postId = fields(PostId)
     // Create a tuple that is our result.
     (voteTypeId, postId)
   }
+
+  
+  
+  /** A function that splits a line of input into (postId, voteTypeId) tuples. */
+  def q2parseLine(line: String) = {
+    val fields = line.split(",")
+    val voteTypeId = fields(VoteTypeId).toInt
+    val postId = fields(PostId).toInt
+    // Create a tuple that is our result.
+    (postId, voteTypeId)
+  }
+
+  
   
     /** Code for Q1 
      *  input - VoteTypeId (key) and PostId
@@ -49,14 +62,79 @@ object Ass3 {
   }
 
   
-  /*
-  def Question2(textFile: RDD[Array[String]]): List[Int] = {
+  def Question2(voteTuple: RDD[(Int,Int)]) : List[Int] = {
 	  //write your code
    
+    // Filter out only the favourite votes
+    val favVotes =  voteTuple.filter(x => x._2 == 5)
+    
+    // Count how many post are ascociated with each VoteTypeId and construct a list of all the post
+    val totalsByVoteTypeId = favVotes.mapValues(x => (x, 1)).reduceByKey( (x,y) => (x._1, x._2 + y._2))
+    // Filter out the ones that don't have a count value > 5
+    val greaterThanFive = totalsByVoteTypeId.filter( x => x._2._2 > 5)
+    // Sord in descending order
+    val sortedResults = greaterThanFive.sortByKey(false)  //takeOrdered(0)(Ordering[Int].reverse.on(_))
+    
+    // Print out the result in the " List" format requested
+    val firstCol = sortedResults.map( x => (x._1) )
+    var isFirstVal = true
+    //firstCol.foreach(println )
+    for(temp<-firstCol)
+    {
+      if( isFirstVal)
+      {
+        isFirstVal = false
+        print("List(")
+      }
+      else
+      {
+        isFirstVal = false
+        print(", ")
+      }
+      
+      print(temp)
+    }
+    print( ")" )
 
-  }*/
+
+    var buf = scala.collection.mutable.ListBuffer[Int]()
+
+    /* TODO Will try an convert the RDD to a list if I get time
+    
+    // Obtain, then Sort and print the final results.
+    //val buf = scala.collection.mutable.ListBuffer.empty[Int]
+
+    for(temp<-firstCol)
+    {
+      println(temp.toInt)
+      buf += temp.toInt //1 Not even having a constant works!
+      println(buf)
+    }
+    
+    // For some reason "buf" gets cleared!!!!
+    println(buf)
 
 
+    
+    //var outList = scala.collection.mutable.Buffer[Int]()
+    //for(str<-firstCol) outList += str
+    val strArray = Array(1,2,3)
+    var outList = scala.collection.mutable.ListBuffer[Int]()
+    
+    for(str<-strArray) // As soon as I change the "strArray" to the firstCol, it prints, but outList is empty?
+    {
+      outList += 1  //str.toInt
+      println(str)
+    }
+    
+    println(outList.toList)
+    */
+    
+    return buf.toList  // This would work if buf wasn't empty!!
+  }
+
+
+  
   def main(args: Array[String]) {
     val inputFile = args(0)    
     val conf = new SparkConf().setAppName("Ass3").setMaster("local")
@@ -64,13 +142,17 @@ object Ass3 {
 
     //val textFile = sc.textFile(inputFile).map(_.split(","))
     val textFile = sc.textFile(inputFile)
-    // Use our parseLines function to convert to (voteTypeId, postId) tuples
-    val voteTuple = textFile.map(parseLine)
+    // Use our q1parseLines function to convert to (voteTypeId, postId) tuples
+    val q1VoteTuple = textFile.map(q1parseLine)
+    
+    val votetype = Question1(q1VoteTuple)
+    println(votetype)
+    
 
     
-    val votetype = Question1(voteTuple)
-    println(votetype)
-    //val posts = Question2(textFile)
+    // Use our q2parseLines function to convert to (voteTypeId, postId) tuples
+    val q2VoteTuple = textFile.map(q2parseLine)
+    val posts = Question2(q2VoteTuple)
     //println(posts)
   }
 }
